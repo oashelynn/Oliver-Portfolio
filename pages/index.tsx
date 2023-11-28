7ce8bfa68022b79ad1f7c8ecb7e8edb548d0cd94
Oliver Cawson <cawsonoliver33@gmail.com>
2023-11-28 10:00:48 +0300

    fix: change meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,12 @@ import Testimonials from "@/components/home/Testimonials";
 
 export default function Home() {
   return (
-    <Page currentPage="Home" meta={{ desc: "I'm a passionate web developer and designer coding beautiful websites and apps." }}>
+    <Page
+      currentPage="Home"
+      meta={{
+        desc: "As a full-stack developer, I'm like a circus performer 🎪, juggling frontend and backend tasks with flair. With the creativity of Picasso 🖌️ and the precision of a Swiss watchmaker ⌚, I bring concepts to life. Turning coffee into code ☕💻 and chaos into order, I tame wild servers and weave intricate webs of logic. Into the bug-hunting jungle 🕷️🌳, I dive with gusto and come out victorious! For me, every line of code is a step towards digital harmony 🎶.",
+      }}
+    >
       <Hero />
       <div className="mt-20 space-y-32">
         <Projects />
